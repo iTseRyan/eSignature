be1ddd6fbcf9e46e26be5388a2deef3c9e579872
Ryan Tse <itseryan@gmail.com>
2019-02-27 15:22:53 -0500

    Update demo files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,9 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes, {
+      useHash: true
+    })
   ],
   exports: [
     RouterModule
